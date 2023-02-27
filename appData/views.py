2b729fbe5f8aed1c5c734bba4b2a24601b581e8b
BiKajoin <witong137@gmail.com>
2023-02-27 16:04:18 +0700

    Add Date filter

--- appData/views.py
+++ appData/views.py
@@ -6,23 +6,36 @@ from appData.models import PVCellData
 from pymongo import MongoClient
 from datetime import datetime
 
+
 # Create your views here.
 @login_required
 def data(request):
-     # Connect to MongoDB
+
+# Connect to MongoDB
     client = MongoClient('mongodb+srv://wannawanna:d1Dj8cOiWwUCIxQs@cluster0.htuap5h.mongodb.net/userdatabase?retryWrites=true&w=majority')
     db = client['userdatabase']
     collection = db['pvcelldata']
 
-    # Retrieve data from MongoDB
-    dt = collection.find().limit(20)
-    #dt.aggregate([{ '$addField': { 'Date': { "$toDate": {"$dateToString":{"datetime":"$clusterTime"}} }, } }])
-    dt = list(dt)
-    for i in range(0,20):
-        timestamp = datetime.fromtimestamp(dt[0]['datetime'].time)
-        print(timestamp)
-        #dt[i]['datetime'] = dt[i]['datetime']*1000
+    startdate = request.GET.get('startdate', '')
+    enddate = request.GET.get('enddate', '')
 
-    # Pass data to template
-    return render(request, 'appData/data.html', {'data': dt})
+    if startdate and enddate:
+        startdate = datetime.strptime(startdate, '%Y-%m-%d')
+        startyear = startdate.year
+        startmonth = startdate.month
+        startday = startdate.day
+
+        enddate = datetime.strptime(enddate, '%Y-%m-%d')
+        endyear = enddate.year
+        endmonth = enddate.month
+        endday = enddate.day
+        
+        filtered_data = collection.find({'year': {'$gte': startyear, '$lte': endyear}, 'month': {'$gte': startmonth, '$lte': endmonth}, 'day': {'$gte': startday, '$lte': endday}})
+        filtered_data_list = list(filtered_data)
+        return render(request, 'appData/filter.html', {'data': filtered_data_list})
+    else:
+        data = collection.find().limit(20)
+        data_list = list(data)
+        return render(request, 'appData/data.html', {'data': data_list})
+
 
