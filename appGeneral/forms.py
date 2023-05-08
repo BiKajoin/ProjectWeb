55ce62e4a971b8f82542b3318bbed938bfd07f08
BiKajoin <witong137@gmail.com>
2023-05-08 14:01:06 +0700

    Add same username handling

--- appGeneral/forms.py
+++ appGeneral/forms.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(AuthenticationForm):
     username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     password=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError('Username already exists.')
